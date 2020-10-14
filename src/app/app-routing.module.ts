0c4a6d6de18a9f211c50a8139f2381d03b0ec1c5
Raul Lora Gomez <lgraul87@gmail.com>
2020-10-15 00:45:47 +0200

    controlc1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 const routes: Routes = [
 
   { path: "inicio", component: InicioComponent },
   { path: "noticias", component: NoticiasComponent },
   { path: "sugerencias", component: SugerenciasComponent },
-  { path: "**", redirectTo: "inicio" }
+  { path: "menu", component: MenuComponent },
+  { path: "**", redirectTo: "menu" }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     NoticiasComponent,
-    SugerenciasComponent
+    SugerenciasComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,

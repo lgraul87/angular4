57de58a9f42d4b30392fad6a756fc46604569fea
Raul Lora Gomez <lgraul87@gmail.com>
2020-10-14 22:26:43 +0200

    iniciobc01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,9 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ]
+  imports: [CommonModule,RouterModule.forRoot(routes)],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component'
     SugerenciasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

9c3b0818e67123df7256c5799854566e87eddc29
Raul Lora Gomez <lgraul87@gmail.com>
2020-10-14 22:12:01 +0200

    iniciob 02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
 
+const routes: Routes =[
+
+  {path:"inicio", component: InicioComponent},
+  {path: "noticias", component: NoticiasComponent},
+  {path: "sugerencias", component: SugerenciasComponent },
+
+];
 
 @NgModule({
   declarations: [],

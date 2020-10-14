9d32004b7ca177f1b5b8692869c2d53a8f5c9bc0
Raul Lora Gomez <lgraul87@gmail.com>
2020-10-14 22:17:08 +0200

    iniciob03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { NoticiasComponent } from './pages/noticias/noticias.component';
 import { SugerenciasComponent } from './pages/sugerencias/sugerencias.component';
 
-const routes: Routes =[
+const routes: Routes = [
 
-  {path:"inicio", component: InicioComponent},
-  {path: "noticias", component: NoticiasComponent},
-  {path: "sugerencias", component: SugerenciasComponent },
+  { path: "inicio", component: InicioComponent },
+  { path: "noticias", component: NoticiasComponent },
+  { path: "sugerencias", component: SugerenciasComponent },
+  { path: "**", redirectTo: "inicio" }
 
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ]
 })
 export class AppRoutingModule { }
